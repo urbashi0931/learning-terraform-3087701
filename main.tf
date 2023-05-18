a344a6abf1dd9a526362f9f9bc9b8b565a332a33
urbashi0931 <61523603+urbashi0931@users.noreply.github.com>
2023-05-18 03:43:16 -0400

    group rule naming

--- main.tf
+++ main.tf
@@ -37,7 +37,7 @@ resource "aws_security_group" "blog"{
   vpc_id= data.aws_vpc.default.id
 }
 
-resource "aws_security_group_rules" "blog_http_in"{
+resource "aws_security_group_rule" "blog_http_in"{
 type       =  "ingress"
 from_port  =  80
 to_port    =  80
@@ -47,7 +47,7 @@ cidr_blocks=["0.0.0.0/0"]
 security_group_id=aws_security_group.blog.id
 }
 
-resource "aws_security_group_rules" "blog_https_in"{
+resource "aws_security_group_rule" "blog_https_in"{
 type       =  "ingress"
 from_port  =  443
 to_port    =  443
@@ -57,7 +57,7 @@ cidr_blocks=["0.0.0.0/0"]
 security_group_id=aws_security_group.blog.id
 }
 
-resource "aws_security_group_rules" "blog_everything_out"{
+resource "aws_security_group_rule" "blog_everything_out"{
 type       =  "egress"
 from_port  =  0
 to_port    =  0
