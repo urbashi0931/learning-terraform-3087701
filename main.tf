84665daf122b4c6f64cc037bab057449df7b21cc
urbashi0931 <61523603+urbashi0931@users.noreply.github.com>
2023-05-18 03:41:58 -0400

    cidr block changes

--- main.tf
+++ main.tf
@@ -42,7 +42,7 @@ type       =  "ingress"
 from_port  =  80
 to_port    =  80
 protocol   =  "tcp"
-cidr_blocks=[0.0.0.0/0]
+cidr_blocks=["0.0.0.0/0"]
 
 security_group_id=aws_security_group.blog.id
 }
@@ -52,7 +52,7 @@ type       =  "ingress"
 from_port  =  443
 to_port    =  443
 protocol   =  "tcp"
-cidr_blocks=[0.0.0.0/0]
+cidr_blocks=["0.0.0.0/0"]
 
 security_group_id=aws_security_group.blog.id
 }
@@ -62,7 +62,7 @@ type       =  "egress"
 from_port  =  0
 to_port    =  0
 protocol   =  "-1"
-cidr_blocks=[0.0.0.0/0]
+cidr_blocks=["0.0.0.0/0"]
 
 security_group_id=aws_security_group.blog.id
 }
